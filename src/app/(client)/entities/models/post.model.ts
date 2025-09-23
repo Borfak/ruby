a4ebc4b536b1d2adb6e7d90b968f2a10b71d35e5
Borfak <borfak@protonmail.com>
2025-09-23 18:52:38 +0300

    refactor: remove unused types

--- src/app/(client)/entities/models/post.model.ts
+++ src/app/(client)/entities/models/post.model.ts
@@ -10,16 +10,6 @@ export interface User {
   name: string
   username: string
   email: string
-  address: {
-    street: string
-    suite: string
-    city: string
-    zipcode: string
-    geo: {
-      lat: string
-      lng: string
-    }
-  }
   phone: string
   website: string
   company: {
